506830ca1bf123b5f321cb5aa6cb3f19fc6d4cc0
Aaron Evans <aarone@outlook.com.au>
2024-10-09 12:56:27 +1100

    Add Ansible inventory output for virtual machines with security profiles and variables

--- output.tf
+++ output.tf
@@ -5,3 +5,20 @@ output "virtual_machine_names" {
     vm_key => vm_value.virtual_machine_name
   }
 }
+
+output "ansible_inventory" {
+  value = {
+    for key, vm in local.vm_config :
+    key => {
+      "hostname"         = vm.hostname
+      "security_profile" = vm.security_profile
+      "group"            = aap_group.vm_groups[vm.security_profile].name
+      "variables"        = {
+        "backup_policy"    : vm.backup_policy,
+        "os_type"          : vm.os_type,
+        "storage_profile"  : vm.storage_profile,
+        "tier"             = vm.tier
+      }
+    }
+  }
+}

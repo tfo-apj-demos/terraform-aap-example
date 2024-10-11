0dc2f3474bf1ff65e911ccd1231bed02f6858f90
Simon Lynch <simon.lynch@hashicorp.com>
2024-10-11 15:11:06 +1100

    Refactor AAP job template to use toset() function for triggers

--- main.tf
+++ main.tf
@@ -58,6 +58,6 @@ resource "aap_job" "vm_demo_job" {
   # })
   
   # Use triggers to control job execution. Trigger if map changes
-   triggers = local.vm_names
+   triggers = toset(local.vm_names)
 
 }

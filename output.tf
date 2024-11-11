c262c26c897e0b9885f0c8c5f14e09b57ceb3ec6
Aaron Evans <aarone@outlook.com.au>
2024-11-12 10:11:35 +1100

    Comment out ansible_inventory output for future reference

--- output.tf
+++ output.tf
@@ -6,19 +6,19 @@ output "virtual_machine_names" {
   }
 }
 
-output "ansible_inventory" {
-  value = {
-    for key, vm in var.vm_config :
-    key => {
-      "hostname"         = vm.hostname
-      "security_profile" = vm.security_profile
-      "group"            = aap_group.vm_groups[vm.security_profile].name
-      "variables"        = {
-        "backup_policy"    : vm.backup_policy,
-        "os_type"          : vm.os_type,
-        "storage_profile"  : vm.storage_profile,
-        "tier"             = vm.tier
-      }
-    }
-  }
-}
+# output "ansible_inventory" {
+#   value = {
+#     for key, vm in var.vm_config :
+#     key => {
+#       "hostname"         = vm.hostname
+#       "security_profile" = vm.security_profile
+#       "group"            = aap_group.vm_groups[vm.security_profile].name
+#       "variables"        = {
+#         "backup_policy"    : vm.backup_policy,
+#         "os_type"          : vm.os_type,
+#         "storage_profile"  : vm.storage_profile,
+#         "tier"             = vm.tier
+#       }
+#     }
+#   }
+# }

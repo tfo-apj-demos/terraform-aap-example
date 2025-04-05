94ec9242b0b7c5a1f7e4f94094f5a2b2ce7f29fc
Aaron Evans <aarone@outlook.com.au>
2025-04-05 22:24:19 +1100

    Remove commented-out variable for Active Directory domain in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,5 @@
 variable "TFC_WORKSPACE_ID" {}
 
-# variable "domain" {
-#   description = "The Active Directory domain for the VMs."
-#   type        = string
-# }
 
 variable "job_template_id" {
   description = "The ID of the job template to run."
